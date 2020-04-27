0462b1e779261f346d801f006ecc216b6bda1343
Félix Urbina <felixurbina@gmx.com>
2020-04-27 08:48:08 +0200

    Carga inicial.

---
+++ o_email_required/README.md
@@ -0,0 +1,3 @@
+### o_email_required
+
+Este módulo hace obligatorio el campo _email_ de _partners_.
---
+++ o_email_required/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'O Email Required',
+    'version': '0.1',
+    'category': 'Hidden',
+    'application': True,
+    'author': 'nuxpy',
+    'contributors': [
+        'Félix Urbina <furbina@nuxpy.com>'
+    ],
+    'website': 'https://nuxpy.com',
+    'summary': 'Make email field as required field.',
+    'description': """This makes the email field as a required field.""",
+    'depends': ['base'],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False
+}
